201276f09bd63fc8a7b0bd8670ee4ccd85a2d73b
Sina Jafari <Sina1382jafari123@gmail.com>
2024-07-01 00:59:50 +0300

    add the custom title bar for windows/linux/macos

---
+++ lib/widgets/window_buttons.dart
@@ -0,0 +1,29 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter/material.dart';
+
+final buttonColors = WindowButtonColors(
+    iconNormal: Colors.lightGreenAccent.shade400,
+    mouseOver: Colors.lightGreenAccent.shade400,
+    mouseDown: Color.fromARGB(255, 80, 179, 19),
+    iconMouseOver: Colors.black,
+    iconMouseDown: Colors.white);
+
+final closeButtonColors = WindowButtonColors(
+    mouseOver: const Color(0xFFD32F2F),
+    mouseDown: const Color(0xFFB71C1C),
+    iconNormal: Colors.lightGreenAccent.shade400,
+    iconMouseOver: Colors.black);
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        MinimizeWindowButton(colors: buttonColors),
+        MaximizeWindowButton(colors: buttonColors),
+        CloseWindowButton(colors: closeButtonColors),
+      ],
+    );
+  }
+}
